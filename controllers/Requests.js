b7a16f1a9e85aacde1e4a5282cb3f2769cfa8dce
Erick Caraballo <erick.caraballo@upr.edu>
2014-04-18 12:17:55 -0400

    Added nodemailer dependency in package.json. Configured it to send email to API key requesters.

--- controllers/Requests.js
+++ controllers/Requests.js
@@ -1,9 +1,23 @@
 var BaseController = require('./Base');
 var View = require('../views/Base');
 
-// A library for parsing, 
-// validating, manipulating, 
-// and formatting dates.
+// For sending emails
+var nodemailer = require('nodemailer');
+// Nodemailer configuration
+var smtpTransport = nodemailer.createTransport("SMTP", {
+	service: "Gmail",
+    auth: {
+		user: "info.trolleytracker@gmail.com",
+        pass: "dn43Lknn*g"
+    }
+});				
+// Email parameters				
+var mailOptions = {	
+	from: "Trolley Tracker <info.trolleytracker@gmail.com>"
+};
+
+// Helps me parse, validate and
+// _format_ dates.
 var moment = require('moment');
 
 //Models
@@ -13,15 +27,18 @@ module.exports = BaseController.extend({
 	name: 'Request API Key',
 	run: function(req, res, next) {
 		
-		//This references to a requests.html file in /templates		
+		// Second parameter references
+		// an html template file in the
+		// templates diretory 
+		// (/templates/requests.html)		
 		var v = new View(res, 'requests'); 
 		v.render({
 			title: 'Request API Key',
 			content: 'Request an API Key',
-			info: 'You need to request an API key in '+
-				  'order to make REST API calls. If your ' + 
-				  'request is approved, a notification will ' +
-				  'be sent to the specified email address.'
+			info: 'You need to request an API key in order ' +
+				  'to make REST API calls. If your request ' + 
+				  'is approved, a notification will be sent' +
+				  'to the specified email address.'
 		});
 		
 	},
@@ -35,9 +52,21 @@ module.exports = BaseController.extend({
 			'disabled': true	
 		};												
 		keyModel.createKey(keyRequest, function(err) {
-			if (!err) {
-				// Debug				
-				console.log('Success: creating key request.');				
+			
+			if (!err) {								
+				// Complete parameters of the email				
+				mailOptions['to'] = keyRequest.email;
+				mailOptions['subject'] = 'API Key - Waiting for Approval';
+    			mailOptions['text'] = 'Thank you. Your API key request is ' +
+    							'waiting approval from our Administrators.' + 
+    							' You will be notified when a decision is ' +
+    							'finally made.';												
+				// Send email
+				smtpTransport.sendMail(mailOptions, function(err, res) {					
+        			if (err) {
+	            		console.log("Error sending message.");
+    	    		}
+        		});	
 			}
 			else { 
 				//TODO: Create another view to display 
